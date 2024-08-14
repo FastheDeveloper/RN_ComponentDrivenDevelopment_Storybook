da4836170d1d1427432823c96a0d8a6d59ddaa94
Farouq Seriki <farouq@basaltx.io>
2024-08-14 23:56:22 +0100

    tailwind setup

--- App.jsx
+++ App.jsx
@@ -4,7 +4,7 @@ import Constants from "expo-constants";
 function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text className='text-red-500'>Open up App.tsx to start working on your app!</Text>
     </View>
   );
 }
