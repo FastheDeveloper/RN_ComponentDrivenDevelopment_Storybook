56b9e9c91796cdb31f7b2a2e0d507ba1d0453004
Farouq Seriki <farouq@basaltx.io>
2024-08-15 00:56:59 +0100

    babel config

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: false,
+  bracketSameLine: false,
+  proseWrap: 'never',
+  printWidth: 120,
+  parser: "typescript"
+}
---
+++ src/screens/Screen1.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Screen1 = () => {
+  return (
+    <View>
+      <Text>Screen1</Text>
+    </View>
+  )
+}
+
+export default Screen1
+
+const styles = StyleSheet.create({})
