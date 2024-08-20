fea58ea10d8d8dbdae4ed7bf5b095d333cc0749d
Farouq Seriki <farouq@basaltx.io>
2024-08-20 14:34:14 +0100

    Button Storybook and jest complete

--- components/Button/AppButton.stories.tsx
+++ components/Button/AppButton.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import AppButton, { Titles } from './AppButton'
-import { FontAwesome } from '@expo/vector-icons'
+import AppButton from './AppButton'
+
 import React from 'react'
 import { View } from 'react-native'
 
@@ -12,7 +12,6 @@ const AppButtonMeta: Meta<typeof AppButton> = {
     onPress: { action: 'pressed the button' },
   },
   args: {
-    title: Titles.BLAND,
     label: 'Story Button',
     loading: false,
     // leftIcon: <FontAwesome name={'paper-plane'} size={20} />,
@@ -29,8 +28,53 @@ const AppButtonMeta: Meta<typeof AppButton> = {
 
 export default AppButtonMeta
 
-export const MainButton: StoryObj<typeof AppButton> = {
-  args: {},
+export const TextOnlyButton: StoryObj<typeof AppButton> = {
+  args: {
+    label: 'Text Button',
+  },
+  argTypes: {
+    onPress: { action: 'Yaay' },
+  },
+  parameters: {
+    noBackground: true,
+  },
+}
+
+export const WithLeftIcon: StoryObj<typeof AppButton> = {
+  args: {
+    label: 'With Left Icon',
+    leftIcon: 'paper-plane',
+  },
+  argTypes: {
+    onPress: { action: 'Lefty Pressed' },
+  },
+  parameters: {
+    noBackground: true,
+  },
+}
+
+export const WithRightIcon: StoryObj<typeof AppButton> = {
+  args: {
+    label: 'With Right Icon',
+    rightIcon: 'user-circle-o',
+  },
+  argTypes: {
+    onPress: { action: 'Lefty Pressed' },
+  },
+  parameters: {
+    noBackground: true,
+  },
+}
+
+export const WithBothIcons: StoryObj<typeof AppButton> = {
+  args: {
+    label: 'With Both Icons',
+    rightIcon: 'user-circle-o',
+    leftIcon: 'paper-plane',
+  },
+  argTypes: {
+    onPress: { action: 'Lefty Pressed' },
+  },
   parameters: {
     noBackground: true,
   },
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './App.{js,jsx,ts,tsx}',
-    './components/**/*.{js,jsx,ts,tsx}',
-    './app/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ['./App.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}', './app/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        APP_COLOR: {
+          MAIN_GREEN: '#209B24',
+          MAIN_GREY_TEXT: '#838282',
+          MAIN_GREY: '#ECECEC',
+          MAIN_WHITE: '#E0E0E0',
+          ACCENT_GREEN: '#4CAF50',
+        },
+      },
+    },
   },
   plugins: [],
-};
+}
