9a8e7c775504f53cf1149868b51996d535d24bf5
Farouq Seriki <farouq@basaltx.io>
2024-08-27 16:34:30 +0100

    Update and tailwind fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ module.exports = {
   content: [
     './App.{js,jsx,ts,tsx}',
     './components/**/*.{js,jsx,ts,tsx}',
-    // './src/**/*.{js,jsx,ts,tsx}',
-    './**/*.{js,jsx,ts,tsx}',
+    './src/**/*.{js,jsx,ts,tsx}',
+    // './**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {
